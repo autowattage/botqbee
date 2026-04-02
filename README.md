51e26a48555aaab2639641d9a75eac6ef7164070
autowattage <pipandw@gmail.com>
2026-04-02 13:57:29 -0500

    reorganize data files, add readme

--- README.md
+++ README.md
@@ -4,5 +4,20 @@ fun litle discord bot i made for me and my friends on sky:cotl!
 features to implement:
 - color nametags
 - birthdays
-- swear jar
 - gambling
+
+current implemented features:
+- swear jar
+
+## how to use
+1. create users.json with `touch users.json`
+  a. this is where saved user data resides
+2. create swearjar.json with `swearjar.json`
+  a. this is where the list of swearwords reside. make an array of swearwords for the bot to detect
+3. create .env with `touch .env && echo "BOT_TOKEN={your token}" > .env`
+4. set up the python virtual environment
+  a. `python3 -m venv bot-env`
+  b. `source bot-env/bin/activate` (use `deactivate` command to exit)
+5. install required packages
+  a. `pip install discord.py`
+  b. `pip install dotenv`
--- main.py
+++ main.py
@@ -9,11 +9,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-# turn swearword.json into editable python dictonary
-with open("swearjar.json", "r") as f:
-    swdata = json.load(f)
-swearwords = swdata["swears"]
-
+with open("swearjar.json", "r") as f: swears = json.load(f)
+with open("users.json", "r") as f: userjson = json.load(f)
+print(userjson)
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
@@ -23,17 +21,15 @@ async def on_message(message):
     if message.author == bot.user: return
     
     # swear jar
-    if any(swear in message.content.lower() for swear in swearwords):
+    if any(word in message.content.lower() for word in swears):
         # update count (i know this is ugly ill fix it later)
-        if message.author.id in swdata["users"]:
-            swdata["users"][message.author.id] += 1
-        else:
-            swcount = swdata["users"].setdefault(message.author.id, 1)
+        userjson.setdefault(str(message.author.id), 0)
+        userjson[str(message.author.id)] += 1
         # writing update to json
-        with open("swearjar.json", "w") as f:
-            json.dump(swdata, f, ensure_ascii=False, indent=3)
-        # warn user
-        await message.channel.send(f"Hey {message.author.name}! Watch your language! You've sworn {swcount} time{"s" if swcount!=1 else ""}!")
+        with open("users.json", "w") as f:
+            json.dump(userjson, f, ensure_ascii=True, indent=3)
+                # warn user
+        await message.channel.send(f"Hey {message.author.name}! Watch your language! You've sworn {userjson[str(message.author.id)]} time{"s" if userjson[str(message.author.id)] !=1 else ""}!")
 
     await bot.process_commands(message)
 
